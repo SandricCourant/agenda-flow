0611e2df9613d14ab122fc992cbada2ab4f88891
NURMAMAT <dilshat917@gmail.com>
2023-06-16 11:05:29 +0200

    navbar

--- front/angular-calendar-interface/src/app/app.module.ts
+++ front/angular-calendar-interface/src/app/app.module.ts
@@ -7,10 +7,12 @@ import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
   ],
   imports: [
     BrowserModule,
