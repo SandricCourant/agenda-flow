f446fe2694d61b195c2746b1a2157c8911634326
NURMAMAT <dilshat917@gmail.com>
2023-06-16 15:56:42 +0200

    Modification navbar

--- front/angular-calendar-interface/src/app/app-routing.module.ts
+++ front/angular-calendar-interface/src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CalendarComponent } from './calendar/calendar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 const routes: Routes = [
   {path: 'calendar', component: CalendarComponent}
